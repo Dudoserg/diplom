baed0c60bc694fb2ddd5377f66120191f3250f13
Dudoser <frielender@gmail.com>
2021-06-01 01:10:34 +0700

    Делаю анализ параметров системы (весов отношений)

--- src/main/java/Perfomance.java
+++ src/main/java/Perfomance.java
@@ -9,31 +9,50 @@ import java.util.*;
 public class Perfomance {
     public static void main(String[] args) throws Exception {
         Perfomance perfomance = new Perfomance();
-        //Set<String> unicalWords = perfomance.calculateUnicalWord();
-        Map<String, Integer> marks = perfomance.readMarks();
-        Map<String, Double> results = perfomance.calculateRating(marks);
+        if(false){
+            //Set<String> unicalWords = perfomance.calculateUnicalWord();
+            Map<String, Integer> marks = perfomance.readMarks();
+            Map<String, Double> results = perfomance.calculateRating(marks);
 
-        List<Pair<String, Double>> list = new ArrayList<>();
-        for (Map.Entry<String, Double> elem : results.entrySet()) {
-            list.add(new Pair<>(elem.getKey(), elem.getValue()));
+            List<Pair<String, Double>> list = new ArrayList<>();
+            for (Map.Entry<String, Double> elem : results.entrySet()) {
+                list.add(new Pair<>(elem.getKey(), elem.getValue()));
+            }
+            Collections.sort(list, (first, second) -> -Double.compare(first.getValue(), second.getValue()));
+
+            for (Pair<String, Double> stringDoublePair : list) {
+                System.out.println(stringDoublePair.getKey() + "\t\t" + stringDoublePair.getValue());
+            }
+
+            System.out.println();
         }
-        Collections.sort(list, (first, second) -> -Double.compare(first.getValue(), second.getValue()));
 
-        for (Pair<String, Double> stringDoublePair : list) {
-            System.out.println(stringDoublePair.getKey() + "\t\t" + stringDoublePair.getValue());
+        ///////////////////////////////////////
+        ///////////////////////////////////////
+        // Извлекаем уникальные слова из результатов
+        Set<String> strings = perfomance.calculateUnicalWord();
+        for (String string : strings) {
+            System.out.println(string);
         }
-
-        System.out.println();
     }
 
-    final String dirPath = "test" + File.separator;
+    final  String dirPath = "C:" + File.separator + "Users" + File.separator + "Dudoser" + File.separator + "Desktop" +
+            File.separator + "results" + File.separator + "test";
 
-    public Set<String> calculateUnicalWord() throws IOException {
+    private boolean isFileExist(String fileName){
+        File tempFile = new File(fileName);
+        return tempFile.exists();
+    }
+
+    public  Set<String> calculateUnicalWord() throws IOException {
         Set<String> wordsSet = new TreeSet<>();
-        for (int a = 10; a < 100; a += 10) {
-            for (int s = 10; s < 100; s += 10) {
-                for (int d = 10; d < 100; d += 10) {
-                    String fileName = dirPath + "A" + a + "_" + "S" + s + "_" + "D" + d + ".txt";
+        for (int a = 0; a < 100; a += 1) {
+            System.out.println("a = " + a);
+            for (int s = 0; s < 100; s += 1) {
+                for (int d = 0; d < 100; d += 1) {
+                    String fileName = dirPath +  File.separator + "A" + a + "_" + "S" + s + "_" + "D" + d + "_cluster.txt";
+                    if( !isFileExist(fileName))
+                        continue;
                     String result = Helper.readFile(fileName);
                     String[] rows = result.split("\n");
                     for (String row : rows) {
