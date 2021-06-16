37b62781f6e294d9469ea76a25708d77d5c70a90
Dudoser <frielender@gmail.com>
2021-06-16 15:26:58 +0700

    (2) Перенес некоторые константы в конфигурационный файл

--- src/main/java/settings/Setting_Base.java
+++ src/main/java/settings/Setting_Base.java
@@ -2,22 +2,16 @@ package settings;
 
 import java.lang.reflect.Field;
 
-public  class Setting_Base {
+public class Setting_Base {
     protected void setField(String fieldName, String value, Class classs) throws IllegalAccessException {
-
         for (Field f : classs.getDeclaredFields()) {
             CustomAnnotation column = f.getAnnotation(CustomAnnotation.class);
             if (column != null) {
-//                System.out.println(column.key());
                 String key = column.key();
                 if (key.equals(fieldName)) {
                     Class<?> type = f.getType();
-                    if (type.equals(int.class)) {
+                    if (type.equals(Integer.class)) {
                         f.set(this, Integer.valueOf(value));
-                    } else if (type.equals(Integer.class)) {
-                        f.set(this, Integer.valueOf(value));
-                    } else if (type.equals(double.class)) {
-                        f.set(this, Double.parseDouble(value));
                     } else if (type.equals(Double.class)) {
                         f.set(this, Double.parseDouble(value));
                     } else if (type.equals(String.class)) {
@@ -26,6 +20,5 @@ public  class Setting_Base {
                 }
             }
         }
-
     }
 }
